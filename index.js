25c85265425c06007797c1647576ba4ddddb95c8
moshupi <moshupi@gmail.com>
2026-04-08 21:22:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,16 @@ const PHONE_NUMBER_ID = process.env.PHONE_NUMBER_ID;
 
 // Your 20 auto-responses go here
 const responses = {
-  "hello": "Hi there! Thanks for reaching out. How can we help you today?",
-  "hours": "We're open Monday–Friday, 8am to 5pm.",
-  "price": "Please visit our website for pricing, or reply with your specific question.",
+  "hello": "Hi. This is D48 MIDRAND! How can we assist? For artists, DJs and job seekers, kindly visit D48 in person to make enquiries there. Strictly. Lastly, please be patient with replies. We will get to your messege. No calls on this number",
+  "Hi": "Hi. This is D48 MIDRAND! How can we assist? For artists, DJs and job seekers, kindly visit D48 in person to make enquiries there. Strictly. Lastly, please be patient with replies. We will get to your messege. No calls on this number",
+  "hours": "We're open Friday - Sunday, 12pm to 3am",
+  "open": "We're open Friday - Sunday, 12pm to 3am",
+  "price": "Food https://d48.co.za/wp-content/uploads/2024/02/D48-Drinks-Food.pdf Drinks https://d48.co.za/wp-content/uploads/2024/02/D48-Drinks-Menu.pdf . Beers and ciders R45. Ice tropez R150. Hubbly is R350",
+  "menu": "Food https://d48.co.za/wp-content/uploads/2024/02/D48-Drinks-Food.pdf Drinks https://d48.co.za/wp-content/uploads/2024/02/D48-Drinks-Menu.pdf . Beers and ciders R45. Ice tropez R150. Hubbly is R350",
+   "reservation": "For us to secure your reservation, may you please send us the following details in this format:\n\nName:\nWhatsApp number:\nNumber of people:\nDate:\nTime of arrival:",
+  "book": "For us to secure your reservation, may you please send us the following details in this format:\n\nName:\nWhatsApp number:\nNumber of people:\nDate:\nTime of arrival:",
+  "table": "For us to secure your reservation, may you please send us the following details in this format:\n\nName:\nWhatsApp number:\nNumber of people:\nDate:\nTime of arrival:",
+  "reserve": "For us to secure your reservation, may you please send us the following details in this format:\n\nName:\nWhatsApp number:\nNumber of people:\nDate:\nTime of arrival:",
   "default": "Thanks for your message! We'll get back to you shortly."
 };
 
